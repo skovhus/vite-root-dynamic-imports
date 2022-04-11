57e928c9c6327795ffa538a200342189d5f96f93
skovhus <kenneth.skovhus@gmail.com>
2022-04-11 22:35:45 +0200

    Show changing root fails node_modules import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  // When root is changed to app, the dynamic imports to node_modules fails
+  // http://localhost:3000/node_modules/i18n-iso-countries/langs/en.json?import fails
+  root: path.resolve(__dirname, './app'),
+
+  plugins: [react()],
 })
